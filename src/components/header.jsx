22caff7bf42593184403010a59244fcd1338fbb6
some-user <yonichoen@tutamail.com>
2024-04-01 19:03:04 -0300

    commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,10 +8,10 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
+
+                <div className="text-animation">
+                  <h1 className="animated-text">              {props.data ? props.data.title : "Loading"}</h1>
+                </div>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
                   href="#features"
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,10 +6,7 @@ export const Services = (props) => {
       <div className="container" style={{backgroundColor:"#454545"}}>
         <div className="section-title">
           <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+
         </div>
         <div className="row">
           {props.data
